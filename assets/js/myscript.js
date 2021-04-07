49342955506c22a615afb955233c07e90947236b
rastislavkopal <rasto.kopal@gmail.com>
2021-04-07 12:00:16 +0200

    init project structure

---
+++ assets/js/myscript.js
@@ -0,0 +1,14 @@
+$(document).ready( function () {
+
+} );
+
+function displayMessage(response)
+{
+    if($('#uploader_div').css('display') != 'none')
+        $('#uploader_div').hide('slow');
+    var message = $('<div class="alert alert-error error-message" style="display: none;">');
+    var close = $('<button type="button" class="close" data-dismiss="alert">&times</button>');
+    message.append(close); // adding the close button to the message
+    message.append(response);
+    message.appendTo($('body')).fadeIn(300).delay(3000).fadeOut(1500);
+}
---
+++ views/footer.php
@@ -0,0 +1,8 @@
+<footer id="indexFooter" class="text-center text-white bg-dark py-3">
+    <div class="row">
+        <div class="text-center p-3 bg-dark col-12">
+            © 2021 Copyright:
+            <a class="text-white mb-3" href="https://mdbootstrap.com/">Rastislav Kopál</a>
+        </div>
+    </div>
+</footer>
